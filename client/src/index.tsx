c1475e88af0aa8e581298cd87f5107c472aedcb2
Adrian Ronquillo <ronquiaj@plu.edu>
2021-06-27 20:33:24 -0500

    Added schema and resolver types to the server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
