a0de663202360ed6c9a78c441f8362d102f5d4c7
Adrian Ronquillo <ronquiaj@plu.edu>
2021-07-21 19:24:01 -0500

    Installed the correct dependencies this time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import GetRandomSong from "./components/GetRandomSong";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <GetRandomSong />
     </div>
   );
