2f91b9c4645cd9cd3cd8c2dca91766dce357a4b9
Adrian Ronquillo <ronquiaj@plu.edu>
2021-07-27 21:20:17 -0500

    Started implemntation of rounds

--- client/src/routes/GamePage/GamePage.tsx
+++ client/src/routes/GamePage/GamePage.tsx
@@ -1,9 +1,6 @@
 import { FC, useState, useEffect, useCallback } from "react";
 import { useLazyQuery } from "@apollo/client";
-import {
-  GetTracks,
-  GetTracks_tracks,
-} from "../../components/__generated__/GetTracks";
+import { GetTracks, GetTracks_tracks } from "../../components/__generated__/GetTracks";
 import { useSong } from "../../contexts/SongContext";
 import Button from "../../components/global/Button/Button";
 import ReadySetGo from "./components/ReadySetGo/ReadySetGo";
@@ -13,11 +10,10 @@ import Typography from "../../components/global/Typography/Typography";
 
 const GamePage: FC = () => {
   const [score, setScore] = useState<number>(0);
-  const [openingCountdownOver, setOpeningCountdownOver] = useState<boolean>(
-    false
-  );
+  const [openingCountdownOver, setOpeningCountdownOver] = useState<boolean>(false);
   const [cacheTracks, setCacheTracks] = useState<GetTracks_tracks[]>([]);
   const [currentTracks, setCurrentTracks] = useState<GetTracks_tracks[]>([]);
+  const [newRound, setNewRound] = useState<boolean>(false);
   const [chosenSong, setChosenSong] = useState<GetTracks_tracks>();
   const [gameStarted, setGameStarted] = useState<boolean>(false);
   const [getTracks, { data }] = useLazyQuery<GetTracks>(GET_TRACKS);
@@ -28,17 +24,17 @@ const GamePage: FC = () => {
 
   // Gets the tracks after the countdown finishes
   useEffect(() => {
-    if (openingCountdownOver) getTracks();
+    if (openingCountdownOver) {
+      getTracks();
+      console.log("getting new tracks");
+    }
   }, [openingCountdownOver, getTracks]);
 
   // When there are tracks coming from the data, then set the tracks to that data. If there is cached tracks, add cached tracks to new data
   const adjustTracks = useCallback(() => {
     if (data?.tracks) {
       cacheTracks.length > 0
-        ? setCacheTracks((cache) => [
-            ...cache,
-            ...(data.tracks as GetTracks_tracks[]),
-          ])
+        ? setCacheTracks((cache) => [...cache, ...(data.tracks as GetTracks_tracks[])])
         : setCacheTracks(data.tracks);
     }
   }, [data?.tracks]);
@@ -50,14 +46,18 @@ const GamePage: FC = () => {
   // Look at our cache of tracks and see if we need to get a fresh set, otherwise take four of the cacheTracks, and set cacheTracks to that
   const getSongAndCache = useCallback(() => {
     if (openingCountdownOver && cacheTracks) {
-      if (cacheTracks.length < 4) getTracks();
-      else {
-        const newTracks = cacheTracks.splice(0, 4);
+      if (cacheTracks.length < 4) {
+        console.log("getting new tracks");
+        getTracks();
+      } else {
+        const newTracks = cacheTracks.slice(0, 4);
         setCurrentTracks(newTracks);
-        const randomSong =
-          newTracks[Math.floor(Math.random() * newTracks.length)];
+        const randomSong = newTracks[Math.floor(Math.random() * newTracks.length)];
         setChosenSong(randomSong);
-        if (setCurrentSong) setCurrentSong(randomSong.preview_url);
+        if (setCurrentSong) setCurrentSong("");
+        setTimeout(() => {
+          if (setCurrentSong) setCurrentSong(randomSong.preview_url);
+        }, 1000);
       }
     }
   }, [cacheTracks, getTracks, openingCountdownOver, setCurrentSong]);
@@ -66,35 +66,53 @@ const GamePage: FC = () => {
     getSongAndCache();
   }, [getSongAndCache]);
 
+  /**
+   * Function passed to buttons. IF this button's song and the current song match, update points and get new tracks
+   * @param buttonSong
+   * @param currentSong
+   */
+  const pickSong = (buttonSong: string, currentSong: string | undefined) => {
+    if (buttonSong === currentSong) {
+      alert("Correct!");
+      setScore((score) => score + 100);
+    } else {
+      alert("Wrong!");
+      setScore((score) => score - 100);
+    }
+    setCacheTracks((cacheTracks) => cacheTracks.slice(4));
+  };
+
+  console.log(cacheTracks);
+
   return (
-    <div className="game-container">
+    <div className='game-container'>
       {gameStarted ? (
         <ReadySetGo
           neon={true}
           countdownWords={["Ready?", "Set...", "Go!"]}
-          className="game-container--ready-set-go"
+          className='game-container--ready-set-go'
           time={1000}
           animationOver={closeOpeningCountdown}
         />
       ) : (
-        <Button className="game-container--big-button" onClick={startGame}>
+        <Button className='game-container--big-button' onClick={startGame}>
           Start
         </Button>
       )}
-      <Typography light={false} variant="hot-pink">
+      <Typography light={false} variant='hot-pink'>
         Score: <span>{score}</span>
       </Typography>
-      <div className="game-container--button-container">
-        <Button onClick={() => alert("Hi")}>
+      <div className='game-container--button-container'>
+        <Button onClick={() => pickSong(currentTracks[0].name, chosenSong?.name)}>
           {currentTracks.length > 0 ? currentTracks[0].name : "Song 1"}
         </Button>
-        <Button onClick={() => alert("Hi")}>
+        <Button onClick={() => pickSong(currentTracks[1].name, chosenSong?.name)}>
           {currentTracks.length > 0 ? currentTracks[1].name : "Song 2"}
         </Button>
-        <Button onClick={() => alert("Hi")}>
+        <Button onClick={() => pickSong(currentTracks[2].name, chosenSong?.name)}>
           {currentTracks.length > 0 ? currentTracks[2].name : "Song 3"}
         </Button>
-        <Button onClick={() => alert("Hi")}>
+        <Button onClick={() => pickSong(currentTracks[3].name, chosenSong?.name)}>
           {currentTracks.length > 0 ? currentTracks[3].name : "Song 4"}
         </Button>
       </div>
